e2aa95cc3d068f7277020bbe8bcd4474c6d8cc9d
lechador <135513703+lechador@users.noreply.github.com>
2023-09-12 19:12:44 +0400

    Update chooseBlock.js

--- app/components/chooseBlock.js
+++ app/components/chooseBlock.js
@@ -7,7 +7,7 @@ export default function ChooseBlock({data}) {
     <div data-theme="garden" className="pt-2 pb-8">
       <ComponentTitle title="აირჩიე ბლოკი" />
       <div className="flex flex-col items-center justify-center lg:flex-row">
-        {data.map((item) => (
+        {data && data.map((item) => (
           <BlockCard key={item._id} block={item.block_name} block_id={item.block_id} status={item.ongoing} aptMax={item.apartmentCount} aptSold={item.soldApartmentCount} image={item.block_image} />
         ))}
       </div>
