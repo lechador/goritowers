63c7fc85ee349eb41c802946801e23bc494fdbe1
lechador <135513703+lechador@users.noreply.github.com>
2023-09-12 19:36:46 +0400

    Update chooseBlock.js

--- app/components/chooseBlock.js
+++ app/components/chooseBlock.js
@@ -1,13 +1,16 @@
+'use client'
+import { useState } from "react";
 import BlockCard from "./blockCard";
 import ComponentTitle from "./componentTitle";
 
 
 export default function ChooseBlock({data}) {
+  const [blocks, setBlocks] = useState(data)
   return (
     <div data-theme="garden" className="pt-2 pb-8">
       <ComponentTitle title="აირჩიე ბლოკი" />
       <div className="flex flex-col items-center justify-center lg:flex-row">
-        {data.map((item) => (
+        {blocks.map((item) => (
           <BlockCard key={item._id} block={item.block_name} block_id={item.block_id} status={item.ongoing} aptMax={item.apartmentCount} aptSold={item.soldApartmentCount} image={item.block_image} />
         ))}
       </div>
