d90f42e2c6565fcb36807076bd49794c56a9bce9
lechador <135513703+lechador@users.noreply.github.com>
2023-09-12 20:11:46 +0400

    updates

--- app/components/chooseBlock.js
+++ app/components/chooseBlock.js
@@ -4,13 +4,14 @@ import ComponentTitle from "./componentTitle";
 
 
 export default async function ChooseBlock() {
-  const {data} = await axios.get(`${process.env.ENV == 'dev' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`}/api/blocks`);
-  const blocks = data.blocks
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV == 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000'}/api/blocks`);
+  const data = await response.data.blocks
+  console.log(data)
   return (
     <div data-theme="garden" className="pt-2 pb-8">
       <ComponentTitle title="აირჩიე ბლოკი" />
       <div className="flex flex-col items-center justify-center lg:flex-row">
-        {blocks && blocks.map((item) => (
+        {data && data.map((item) => (
           <BlockCard key={item._id} block={item.block_name} block_id={item.block_id} status={item.ongoing} aptMax={item.apartmentCount} aptSold={item.soldApartmentCount} image={item.block_image} />
         ))}
       </div>
