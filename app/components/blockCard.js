594eb74101aadc3761c411ad8c842559d80ccbfa
lechador <135513703+lechador@users.noreply.github.com>
2023-09-13 13:06:27 +0400

    optimized routing

--- app/components/blockCard.js
+++ app/components/blockCard.js
@@ -1,22 +1,19 @@
-'use client'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link';
 
 export default function BlockCard({block, status, aptMax, aptSold, image, block_id}) {
-  const router = useRouter();
   return (
-    <div 
-      className="card w-80 md:w-96 bg-base-100 shadow-xl image-full lg:mx-4 my-4 cursor-pointer"
-      onClick={() => router.push(`/project/${block_id}`)}
-    >
-        <figure><img src={image} alt="image" /></figure>
-        <div className="card-body">
-            <h2 className="card-title font-bold text-2xl">{block} ბლოკი</h2>
-            <p className="text-orange-400 font-bold">{status ? 'მიმდინარე' : 'დასრულებული'}</p>
-            <div className="card-actions">
-                <span className="w-full flex justify-center text-xl text-pink-300">დარჩენილია {aptMax-aptSold} ბინა</span>
-                <progress className="progress progress-success w-full" value={aptSold} max={aptMax}></progress>
-            </div>
+      <Link href={`/project/${block_id}`}>
+        <div className="card w-80 md:w-96 bg-base-100 shadow-xl image-full lg:mx-4 my-4 cursor-pointer">
+          <figure><img src={image} alt="image" /></figure>
+          <div className="card-body">
+              <h2 className="card-title font-bold text-2xl">{block} ბლოკი</h2>
+              <p className="text-orange-400 font-bold">{status ? 'მიმდინარე' : 'დასრულებული'}</p>
+              <div className="card-actions">
+                  <span className="w-full flex justify-center text-xl text-pink-300">დარჩენილია {aptMax-aptSold} ბინა</span>
+                  <progress className="progress progress-success w-full" value={aptSold} max={aptMax}></progress>
+              </div>
+          </div>
         </div>
-    </div>
+        </Link>
   )
 }
--- app/components/callModal.js
+++ app/components/callModal.js
@@ -5,7 +5,7 @@ import RequestCall from "./requestCall"
 export default function CallModal() {
   return (
     <div className="navbar-end">
-        <a className="btn bg-defaultOrange text-white" onClick={()=>window.my_modal_2.showModal()}>შეუკვეთე ზარი</a>
+        <a className="btn bg-defaultOrange text-white hover:bg-defaultOrange" onClick={()=>window.my_modal_2.showModal()}>შეუკვეთე ზარი</a>
         <dialog id="my_modal_2" className="modal">
         <form method="dialog" className="modal-box w-11/12 max-w-5xl" data-theme="dark" >
             <RequestCall showTitle={false} theme={'dark'} /> 
