f891dd3343323675c96a9526fe597529d9de9dcd
lechador <135513703+lechador@users.noreply.github.com>
2023-11-07 19:03:16 +0400

    updates

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -9,49 +10,26 @@ export default function Footer() {
         </div>
         <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
           <div className="space-y-3">
-            <h3 className="tracki uppercase dark:text-gray-50">Product</h3>
+            <h3 className="tracki uppercase dark:text-gray-50">პროექტები</h3>
             <ul className="space-y-1">
               <li>
-                <a rel="noopener noreferrer" href="#">Features</a>
-              </li>
-              <li>
-                <a rel="noopener noreferrer" href="#">Integrations</a>
-              </li>
-              <li>
-                <a rel="noopener noreferrer" href="#">Pricing</a>
-              </li>
-              <li>
-                <a rel="noopener noreferrer" href="#">FAQ</a>
+                <Link href="/project">გორი თაუერსი</Link>
               </li>
             </ul>
           </div>
           <div className="space-y-3">
-            <h3 className="tracki uppercase dark:text-gray-50">Company</h3>
+            <h3 className="tracki uppercase dark:text-gray-50">კომპანია</h3>
             <ul className="space-y-1">
               <li>
-                <a rel="noopener noreferrer" href="#">Privacy</a>
+                <Link href="/about">ჩვენს შესახებ</Link>
               </li>
               <li>
-                <a rel="noopener noreferrer" href="#">Terms of Service</a>
+                <Link href="/contact">კონტაქტი</Link>
               </li>
             </ul>
           </div>
           <div className="space-y-3">
-            <h3 className="uppercase dark:text-gray-50">Developers</h3>
-            <ul className="space-y-1">
-              <li>
-                <a rel="noopener noreferrer" href="#">Public API</a>
-              </li>
-              <li>
-                <a rel="noopener noreferrer" href="#">Documentation</a>
-              </li>
-              <li>
-                <a rel="noopener noreferrer" href="#">Guides</a>
-              </li>
-            </ul>
-          </div>
-          <div className="space-y-3">
-            <div className="uppercase dark:text-gray-50">Social media</div>
+            <div className="uppercase dark:text-gray-50">მოგვძებნე ინტერნეტში</div>
             <div className="flex justify-start space-x-3">
               <a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
--- app/components/textComponent.js
+++ app/components/textComponent.js
@@ -2,7 +2,7 @@
 
 export default function TextComponent({text, theme}) {
   return (
-    <div className="flex justify-center items-center pt-10 pb-10" data-theme={theme}>
+    <div className="flex justify-center items-center py-6" data-theme={theme}>
         <p className="text-center text-lg mx-3 lg:mx-40">
             {text}
         </p>
