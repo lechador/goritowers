6c9563ec0e8f76d503699b8f9fc01a91e768987e
lechador <135513703+lechador@users.noreply.github.com>
2023-11-12 14:38:48 +0400

    Update mobileFloor1.js

--- app/polygons/mobileFloor1.js
+++ app/polygons/mobileFloor1.js
@@ -1,18 +1,17 @@
-const hoverColor = 'rgba(255, 88, 1, 0.6)';
+const hoverColor = 'rgba(169, 136, 153, 0.7)';
 export const mobileFloor1 = [
     {
       points: [
-        { x: 5, y: 290 },
-        { x: 116, y: 289 },
-        { x: 116, y: 390 },
-        { x: 100, y: 391 },
-        { x: 100, y: 361 },
-        { x: 27, y: 361 },
-        { x: 26, y: 370 },
-        { x: 7, y: 370 }        
+        { x: 4, y: 168 },
+        { x: 68, y: 171 },
+        { x: 68, y: 228 },
+        { x: 58, y: 228 },
+        { x: 57, y: 211 },
+        { x: 18, y: 213 },
+        { x: 5, y: 215 }     
       ],
       fill: hoverColor,
       selectable: false,
-      opacity: 0
+      opacity: 1
     },
 ]
