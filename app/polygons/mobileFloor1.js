705776abc27c9e71c4bdd1352f589cd2b178c2bc
lechador <135513703+lechador@users.noreply.github.com>
2023-11-12 15:11:14 +0400

    Update mobileFloor1.js

--- app/polygons/mobileFloor1.js
+++ app/polygons/mobileFloor1.js
@@ -12,6 +12,162 @@ export const mobileFloor1 = [
       ],
       fill: hoverColor,
       selectable: false,
-      opacity: 1
+      opacity: 0
     },
+    {
+        points: [
+            { x: 4, y: 216 },
+            { x: 16, y: 216 },
+            { x: 54, y: 214 },
+            { x: 54, y: 242 },
+            { x: 65, y: 244 },
+            { x: 66, y: 301 },
+            { x: 59, y: 301 },
+            { x: 58, y: 311 },
+            { x: 8, y: 311 } 
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 67, y: 163 },
+            { x: 118, y: 161 },
+            { x: 117, y: 226 },
+            { x: 68, y: 227 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 69, y: 243 },
+            { x: 118, y: 246 },
+            { x: 117, y: 313 },
+            { x: 74, y: 312 },
+            { x: 70, y: 299 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 120, y: 170 },
+            { x: 168, y: 170 },
+            { x: 168, y: 228 },
+            { x: 120, y: 227 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 120, y: 246 },
+            { x: 167, y: 246 },
+            { x: 167, y: 310 },
+            { x: 121, y: 310 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 173, y: 246 },
+            { x: 220, y: 246 },
+            { x: 220, y: 310 },
+            { x: 174, y: 310 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 222, y: 245 },
+            { x: 247, y: 245 },
+            { x: 249, y: 216 },
+            { x: 316, y: 214 },
+            { x: 317, y: 314 },
+            { x: 222, y: 314 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 250, y: 163 },
+            { x: 318, y: 164 },
+            { x: 317, y: 212 },
+            { x: 250, y: 212 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 250, y: 113 },
+            { x: 318, y: 114 },
+            { x: 317, y: 162 },
+            { x: 250, y: 162 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 174, y: 102 },
+            { x: 233, y: 101 },
+            { x: 233, y: 161 },
+            { x: 174, y: 161 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 249, y: 63 },
+            { x: 307, y: 62 },
+            { x: 308, y: 114 },
+            { x: 250, y: 111 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 166, y: 11 },
+            { x: 214, y: 11 },
+            { x: 214, y: 34 },
+            { x: 236, y: 35 },
+            { x: 233, y: 56 },
+            { x: 175, y: 62 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
+      {
+        points: [
+            { x: 219, y: 10 },
+            { x: 220, y: 31 },
+            { x: 238, y: 32 },
+            { x: 241, y: 43 },
+            { x: 251, y: 57 },
+            { x: 305, y: 56 },
+            { x: 306, y: 10 }
+        ],
+        fill: hoverColor,
+        selectable: false,
+        opacity: 0
+      },
 ]
