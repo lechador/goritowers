ae03a0f0f1a5de54bb50b94fcedaebae4c8dff3b
lechador <135513703+lechador@users.noreply.github.com>
2024-04-21 03:34:40 +0400

    Update apartmentBody.js

--- app/components/apartmentBody.js
+++ app/components/apartmentBody.js
@@ -2,6 +2,42 @@
 export default function ApartmentBody({ apartment_area, living_area, balcony_area, apartment_render_image, living_room_and_kitchen_area, toilet_area, bedroom_area, second_bedroom_area, third_bedroom_area }) {
   return (
     <div className="flex flex-col md:flex-row md:px-10 w-full justify-around items-center py-6">
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal" role="dialog" style={{ zIndex: '1001' }}>
+        <div className="modal-box !max-h-screen h-screen max-w-fit py-0">
+          <div className="flex flex-col md:flex-row"> 
+            <img src={apartment_render_image} alt="image" className="md:h-screen" />
+            <div className="flex flex-col justify-center"> 
+              <div className="flex flex-col mb-4 text-xl"> 
+                <span>ბინის ფართი: {apartment_area} მ<sup>2</sup></span>
+                <span>საცხოვრებელი ფართი: {living_area} მ<sup>2</sup></span>
+                <span>აივნის ფართი: {balcony_area} მ<sup>2</sup></span>
+              </div>
+              <hr />
+              <div className="flex flex-col mt-4 text-xl">
+                <span>მისაღები / სამზარეულო: {living_room_and_kitchen_area}მ<sup>2</sup></span>
+                <span>სველი წერტილი: {toilet_area} მ<sup>2</sup></span>
+                <span>საძინებელი: {bedroom_area} მ<sup>2</sup></span>
+                {
+                  second_bedroom_area && second_bedroom_area > 0 ? (
+                    <span>მეორე საძინებელი: {second_bedroom_area} მ<sup>2</sup></span>
+                  ) : (
+                    <span></span>
+                  )
+                }
+                {
+                  third_bedroom_area && third_bedroom_area > 0 ? (
+                    <span>მესამე საძინებელი: {third_bedroom_area} მ<sup>2</sup></span>
+                  ) : (
+                    <span></span>
+                  )
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+        <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+      </div>
         <div className="w-full md:w-4/6">
           <div className="card w-full shadow-xl my-4" data-theme="dark">
             <div className="card-body">
@@ -58,7 +94,9 @@ export default function ApartmentBody({ apartment_area, living_area, balcony_are
           </div>
         </div>
         <div className="w-full md:w-2/6">
-          <img src={apartment_render_image} alt="image" />
+          <label htmlFor="my_modal_7" className="btn">
+            <img src={apartment_render_image} alt="image" />
+          </label>
         </div>
       </div>
   )
