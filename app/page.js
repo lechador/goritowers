b3046d7c1792087db06d4a274cf8feb3e62eccfc
lechador <135513703+lechador@users.noreply.github.com>
2024-06-30 13:01:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home(){
         <Header />
         <Hero />
         <HomeOngoingProject />
-        <LoanCalc />
+        {/* <LoanCalc /> */}
         {/* <Faq /> */}
         <RequestCall showTitle={true} theme={'garden'} />
         <Footer /> 
