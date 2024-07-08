9b61a2553feecb109564f18dba4549c1c55f540c
lechador <135513703+lechador@users.noreply.github.com>
2024-07-08 14:14:26 +0200

    Update fbchat.js

--- app/components/fbchat.js
+++ app/components/fbchat.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 export default function Fbchat() {
   return (
-    <FacebookProvider appId="1192887195084913" chatSupport>
+    <FacebookProvider appId="1006483274309333" chatSupport>
         <CustomChat pageId="366838836504467" minimized={false}/>
       </FacebookProvider>      
   )
