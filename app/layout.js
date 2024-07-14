ba6480ac37c999b0d627488ebafc97057aae19ad
lechador <135513703+lechador@users.noreply.github.com>
2024-07-14 14:14:59 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import 'leaflet/dist/leaflet.css'
 import 'react-toastify/dist/ReactToastify.css';
 import Nprogress from './components/nprogress';
 import TawkChat from './components/tawk';
+import Script from 'next/script';
+import Head from 'next/head';
 
 const ninoMtavruli = localFont({
   src: './bpg_nino_mtavruli_normal.woff2',
@@ -24,6 +26,24 @@ export default function RootLayout({ children }) {
     <html lang="ka" data-theme="light">
       <body className={ninoMtavruli.className}>
         {children}
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1000407334820321');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1000407334820321&ev=PageView&noscript=1" />
+        </noscript>
         <Nprogress />
         <ToastContainer />
         <TawkChat />
