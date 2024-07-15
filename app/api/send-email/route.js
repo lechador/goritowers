89d0c495934efdc0fb1e32d3cd079cb21a100e0d
lechador <135513703+lechador@users.noreply.github.com>
2024-07-15 13:25:26 +0400

    Update route.js

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -2,33 +2,34 @@ import { NextResponse } from "next/server";
 import nodemailer from 'nodemailer';
 
 export async function POST(request) {
-  
     const req = await request.json();
     const transporter = nodemailer.createTransport({
-        service: 'AOL',
+        host: 'smtp.sendgrid.net', 
+        port: 465,
+        secure: true,
         auth: {
-            user: 'egivi@aol.com',
-            pass: 'ltnzssqsgcazoetj'
+            user: 'apikey',
+            pass: process.env.SENDGRID_PASS
         }
     });
 
-  try {
-    await transporter.sendMail({
-      from: `გორითაუერსი <egivi@aol.com>`,
-      to: [
-        'welcome@goritowers.ge'
-        ],
-      subject: 'ზარის შეკვეთა',
-      text: `
-        სახელი: ${req.name},
-        ტელეფონის ნომერი: ${req.number},
-        ზარის შეკვეთის მიზანი: ${req.reason}
-      `
-    });
+    try {
+        await transporter.sendMail({
+            from: `გორითაუერსი <info@goritowers.ge>`,
+            to: [
+                'info@goritowers.ge'
+            ],
+            subject: 'ზარის შეკვეთა',
+            text: `
+                სახელი: ${req.name},
+                ტელეფონის ნომერი: ${req.number},
+                ზარის შეკვეთის მიზანი: ${req.reason}
+            `
+        });
 
-    return NextResponse.json({ message: 'Email sent successfully!' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return NextResponse.json({ message: 'Failed to send email.' });
-  }
+        return NextResponse.json({ message: 'Email sent successfully!' });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return NextResponse.json({ message: 'Failed to send email.' });
+    }
 }
