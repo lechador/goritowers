c88654a1053a85f99cdb81ec0d34a63a893a3bc9
lechador <135513703+lechador@users.noreply.github.com>
2024-07-21 16:49:02 +0400

    Update client.js

--- app/[locale]/test/client.js
+++ app/[locale]/test/client.js
@@ -1,80 +1,85 @@
-'use client'
-import { useState } from 'react';
-import axios from 'axios';
+// 'use client'
+// import { useState } from 'react';
+// import axios from 'axios';
 
-export default function ClientRegister() {
-    const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+// export default function ClientRegister() {
+//     const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+//   const handleEmailChange = (e) => {
+//     setEmail(e.target.value);
+//   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("https://goritowers.ge/api/register", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              email: email,
-              password: password,
-          }),
-      });
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch("https://goritowers.ge/api/register", {
+//           method: "POST",
+//           headers: {
+//               "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({
+//               email: email,
+//               password: password,
+//           }),
+//       });
   
-      if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+//       if (!response.ok) {
+//           throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
   
-      const data = await response.json();
-      console.log(data);
-  } catch (error) {
-      console.log(error);
-  }
+//       const data = await response.json();
+//       console.log(data);
+//   } catch (error) {
+//       console.log(error);
+//   }
   
-  };
+//   };
+//   return (
+//     <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+//         <div className="mb-4">
+//           <label htmlFor="email" className="text-sm font-medium text-gray-600">
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             id="email"
+//             name="email"
+//             value={email}
+//             onChange={handleEmailChange}
+//             className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//             required
+//           />
+//         </div>
+//         <div className="mb-4">
+//           <label htmlFor="password" className="text-sm font-medium text-gray-600">
+//             Password
+//           </label>
+//           <input
+//             type="password"
+//             id="password"
+//             name="password"
+//             value={password}
+//             onChange={handlePasswordChange}
+//             className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//             required
+//           />
+//         </div>
+//         <button
+//           type="submit"
+//           className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+//         >
+//           Register
+//         </button>
+//       </form>
+//   )
+// }
+export default function ClientReg() {
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-        <div className="mb-4">
-          <label htmlFor="email" className="text-sm font-medium text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="text-sm font-medium text-gray-600">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        >
-          Register
-        </button>
-      </form>
+    <div>client</div>
   )
 }
