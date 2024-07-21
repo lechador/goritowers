2ab37a711ee24f11c2d760666ae8c0c6fa2b67b7
lechador <135513703+lechador@users.noreply.github.com>
2024-07-21 16:41:12 +0400

    updates

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,4 @@
-import {withAuth} from 'next-auth/middleware';
-import createMiddleware from 'next-intl/middleware';
-import {NextRequest} from 'next/server';
-
-const locales = ['ka', 'en', 'ru']
-const publicPages = ['/', '/login', '/test', '/project', '/about', '/contact'];
- 
-const intlMiddleware = createMiddleware({
-  locales: locales,
-  defaultLocale: 'ka', 
-  localeDetection: false
-});
- 
-const authMiddleware = withAuth(
-  function onSuccess(req) {
-    return intlMiddleware(req);
-  },
-  {
-    callbacks: {
-      authorized: ({token}) => token != null
-    },
-    pages: {
-      signIn: '/login'
-    }
-  }
-);
- 
-export default function middleware(req: NextRequest) {
-  const publicPathnameRegex = RegExp(
-    `^(/(${locales.join('|')}))?(${publicPages
-      .flatMap((p) => (p === '/' ? ['', '/'] : p))
-      .join('|')})/?$`,
-    'i'
-  );
-  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
- 
-  if (isPublicPage) {
-    return intlMiddleware(req);
-  } else {
-    return (authMiddleware as any)(req);
-  }
+export {default} from "next-auth/middleware"
+export const config = { 
+    matcher: ["/admin/:path*"]
 }
- 
-export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)']
-};
