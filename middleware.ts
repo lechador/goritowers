e4dd6090c4e2d28dfa338e6d0ce4392cea15400c
lechador <135513703+lechador@users.noreply.github.com>
2024-07-21 17:00:11 +0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,39 @@
-import {withAuth} from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
 import createMiddleware from 'next-intl/middleware';
-import {NextRequest} from 'next/server';
-const locales = ['ka', 'en', 'ru']
- 
-const publicPages = ['/', '/about', '/contact', '/project'];
- 
+import { NextRequest } from 'next/server';
+
+const locales = ['ka', 'en', 'ru'];
+
 const intlMiddleware = createMiddleware({
-  locales,
-  defaultLocale: 'en'
+  locales: locales,
+  defaultLocale: 'ka',
+  localeDetection: false
 });
- 
+
 const authMiddleware = withAuth(
-  // Note that this callback is only invoked if
-  // the `authorized` callback has returned `true`
-  // and not for pages listed in `pages`.
   function onSuccess(req) {
     return intlMiddleware(req);
   },
   {
     callbacks: {
-      authorized: ({token}) => token != null
+      authorized: ({ token }) => token != null
     },
     pages: {
       signIn: '/login'
     }
   }
 );
- 
+
 export default function middleware(req: NextRequest) {
-  const publicPathnameRegex = RegExp(
-    `^(/(${locales.join('|')}))?(${publicPages
-      .flatMap((p) => (p === '/' ? ['', '/'] : p))
-      .join('|')})/?$`,
-    'i'
-  );
-  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
- 
-  if (isPublicPage) {
-    return intlMiddleware(req);
-  } else {
+  const isAdminPage = req.nextUrl.pathname.startsWith('/admin');
+
+  if (isAdminPage) {
     return (authMiddleware as any)(req);
+  } else {
+    return intlMiddleware(req);
   }
 }
- 
+
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)']
 };
